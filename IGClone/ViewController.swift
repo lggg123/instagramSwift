dd833ed8416ac7f91c595033a9d538127f58cdf7
George Lugo <machinelearner2334@gmail.com>
2022-01-10 07:20:29 -0800

    Initial Commit

---
+++ IGClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IGClone
+//
+//  Created by George Lugo on 1/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
