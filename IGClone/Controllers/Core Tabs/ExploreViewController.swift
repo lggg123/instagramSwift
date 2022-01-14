20691a0354688ce4c56cd0b3a894c514b804be62
George Lugo <machinelearner2334@gmail.com>
2022-01-14 07:08:31 -0800

    Initial architecture

---
+++ IGClone/Controllers/Core Tabs/ExploreViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreViewController.swift
+//  IGClone
+//
+//  Created by George Lugo on 1/10/22.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ IGClone/Controllers/Other/PostEditorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PostEditorViewController.swift
+//  IGClone
+//
+//  Created by George Lugo on 1/14/22.
+//
+
+import UIKit
+
+class PostEditorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
